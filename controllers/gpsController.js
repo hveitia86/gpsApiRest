6e72ba9a2fc68460d52212cc2ad6b121d9d19849
hveitia86 <hveitia86@gmail.com>
2016-06-05 15:47:27 -0500

    Eliminar todas las coordenas

--- controllers/gpsController.js
+++ controllers/gpsController.js
@@ -5,70 +5,83 @@ var GPS  = mongoose.model('GPS');
 //GET - Return all coordinates in the DB
 exports.findAllGPS = function(req, res) {
 
-    GPS.find(function(err, gpsCoords) {
+  GPS.find(function(err, gpsCoords) {
 
     if(err) res.send(500, err.message);
 
     console.log('GET /gpsCoords')
-        res.status(200).jsonp(gpsCoords);
-    });
+    res.status(200).jsonp(gpsCoords);
+  });
 };
 
 //GET - Return all Coordinates with specified phone
 exports.findByPhoneNumber = function(req, res) {
 
-    GPS.find({cellNumber: req.params.phone}, function(err, gpsCoords) {
+  GPS.find({cellNumber: req.params.phone}, function(err, gpsCoords) {
 
     if(err){
-    	return res.send(500, err.message);
+      return res.send(500, err.message);
     }
-        res.status(200).jsonp(gpsCoords);
-    });
+    res.status(200).jsonp(gpsCoords);
+  });
 };
 
 //POST - Insert a new Coordinate in the DB
 exports.addGPSCoord = function(req, res) {
 
-    console.log('POST');
-    console.log(req.body);
+  console.log('POST');
+  console.log(req.body);
 
-    var gpsCoord = new GPS({
+  var gpsCoord = new GPS({
 
-      cellNumber:    req.body.cellNumber,
-      date:          req.body.date,
-      time:          req.body.time,
-      long:          req.body.long,
-      lat:           req.body.lat,
+    cellNumber:    req.body.cellNumber,
+    date:          req.body.date,
+    time:          req.body.time,
+    long:          req.body.long,
+    lat:           req.body.lat,
 
-    });
+  });
 
-    gpsCoord.save(function(err, gpsCoord) {
-        if(err) return res.send(500, err.message);
+  gpsCoord.save(function(err, gpsCoord) {
+    if(err) return res.send(500, err.message);
     res.status(200).jsonp(gpsCoord);
-    });
+  });
 };
 
 //OPTIONS Allow CORS to DELETE
 exports.optionGpsCoords = function(req, res, next) {
 
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'DELETE');
-	res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
 
-	next();
+  next();
 }
 
 //DELETE Delete gpsCoord
 exports.deleteGpsCoords = function(req, res){
 
+  if(req.params.id == 'all'){
+
+    GPS.remove(function(err) {
+
+      if(err) return res.status(500).send(err.message);
+
+      res.status(200).jsonp('OK');
+    })
+
+  }else{
     GPS.findById(req.params.id, function(err, gpsCoord) {
 
-        gpsCoord.remove(function(err) {
+      gpsCoord.remove(function(err) {
 
-            if(err) return res.status(500).send(err.message);
+        if(err) return res.status(500).send(err.message);
 
-      		res.status(200).jsonp('OK');
-        })
+        res.status(200).jsonp('OK');
+      })
     });
+  }
+
+
 
 };
